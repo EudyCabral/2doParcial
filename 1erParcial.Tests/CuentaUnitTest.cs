3fee1bc5052187deaf35a6e1ac74881000de6bd8
Eudy Cabral Burgos <eudy_cabral@ucne.edu.do>
2019-02-06 01:03:46 -0400

    Test Listo

---
+++ 1erParcial.Tests/CuentaUnitTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using BLL;
+using Entidades;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _1erParcial.Tests
+{
+    [TestClass]
+    public class CuentaUnitTest
+    {
+        private CuentasBancarias GetCuentas()
+        {
+            CuentasBancarias cuentasbancarias = new CuentasBancarias();
+
+            cuentasbancarias.CuentaId = 0;
+            cuentasbancarias.Nombre = "Ventas de Software";
+            cuentasbancarias.Fecha = DateTime.Now;
+            cuentasbancarias.Balance = 4000;
+            return cuentasbancarias;
+        }
+        Repositorio<CuentasBancarias> repositorio = new Repositorio<CuentasBancarias>();
+
+        [TestMethod]
+        public void Guardar()
+        {
+            
+           Assert.IsTrue(repositorio.Guardar(GetCuentas()));
+           
+        }
+
+        [TestMethod]
+        public void Modificar()
+        {
+          
+            Assert.IsTrue(repositorio.Modificar(GetCuentas()));
+
+        }
+
+        [TestMethod]
+        public void Buscar()
+        {
+            Assert.IsNotNull(repositorio.Buscar(1));
+
+        }
+
+     
+        [TestMethod]
+        public void GetList()
+        {
+            var lista = repositorio.GetList(x => true);
+            Assert.IsNotNull(lista);
+
+        }
+
+        [TestMethod]
+        public void Eliminar()
+        {
+            Assert.IsNotNull(repositorio.Eliminar(1));
+        }
+
+    }
+}
---
+++ 1erParcial.Tests/DepositoTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using BLL;
+using Entidades;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _1erParcial.Tests
+{
+    [TestClass]
+    public class DepositoTest
+    {
+        private Depositos GetDepositos()
+        {
+            Depositos depositos = new Depositos();
+
+            depositos.DepositoId = 1;
+            depositos.Concepto = "Ventas de SoftDrink";
+            depositos.Fecha = DateTime.Now;
+            depositos.CuentaId= 2;
+            depositos.Monto = 1000;
+            return depositos;
+        }
+
+        RepositorioDepositos repositorio = new RepositorioDepositos();
+
+        [TestMethod]
+        public void Guardar()
+        {
+
+            Assert.IsTrue(repositorio.Guardar(GetDepositos()));
+
+        }
+
+        [TestMethod]
+        public void Modificar()
+        {
+
+            Assert.IsTrue(repositorio.Modificar(GetDepositos()));
+
+        }
+
+        [TestMethod]
+        public void Buscar()
+        {
+            Assert.IsNotNull(repositorio.Buscar(1));
+
+        }
+
+
+        [TestMethod]
+        public void GetList()
+        {
+            var lista = repositorio.GetList(x => true);
+            Assert.IsNotNull(lista);
+
+        }
+
+        [TestMethod]
+        public void Eliminar()
+        {
+            Assert.IsNotNull(repositorio.Eliminar(1));
+        }
+    }
+}
